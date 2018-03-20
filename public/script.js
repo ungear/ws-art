04acf323ca5c319eea0aed02bc4ebd4c004f0bbd
Mikhail Istomin <mikhail_istomin@epam.com>
2018-03-20 22:15:11 +0300

    Client: clean up

--- public/script.js
+++ public/script.js
@@ -81,26 +81,4 @@
   function onNewBitMessageReceived(event) {
     addBitOnCanvas(JSON.parse(event.data))
   };
-  //var socket = new WebSocket("ws://localhost:8081");
-  /*
-  // отправить сообщение из формы publish
-  document.forms.publish.onsubmit = function() {
-    var outgoingMessage = this.message.value;
-  
-    socket.send(outgoingMessage);
-    return false;
-  };
-  
-  // обработчик входящих сообщений
-  socket.onmessage = function(event) {
-    var incomingMessage = event.data;
-    showMessage(incomingMessage);
-  };
-  
-  // показать сообщение в div#subscribe
-  function showMessage(message) {
-    var messageElem = document.createElement('div');
-    messageElem.appendChild(document.createTextNode(message));
-    document.getElementById('subscribe').appendChild(messageElem);
-  }*/
 })()
