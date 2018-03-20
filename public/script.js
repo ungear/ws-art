bf125c306b8c64d40eb860ab54eaff4eca64f993
Mikhail Istomin <mikhail_istomin@epam.com>
2018-03-20 20:35:59 +0300

    basic web socket workflow

--- public/script.js
+++ public/script.js
@@ -1,3 +1,24 @@
 (function(){
-  console.log(2)
+  var socket = new WebSocket("ws://localhost:8081");
+  
+  // отправить сообщение из формы publish
+  document.forms.publish.onsubmit = function() {
+    var outgoingMessage = this.message.value;
+  
+    socket.send(outgoingMessage);
+    return false;
+  };
+  
+  // обработчик входящих сообщений
+  socket.onmessage = function(event) {
+    var incomingMessage = event.data;
+    showMessage(incomingMessage);
+  };
+  
+  // показать сообщение в div#subscribe
+  function showMessage(message) {
+    var messageElem = document.createElement('div');
+    messageElem.appendChild(document.createTextNode(message));
+    document.getElementById('subscribe').appendChild(messageElem);
+  }
 })()
--- server.js
+++ server.js
@@ -1,9 +1,37 @@
-var express = require('express');
+const express = require('express');
+const WebSocketServer = new require('ws');
 const app = express();
 app.use(express.static(__dirname + '/public'));
 
-const port = 3002;
-const server = app.listen(port, function () {
-  console.dir(server.address())
+// http stuff
+const httpPort = 3002;
+const server = app.listen(httpPort, function () {
   console.log(`visit http://localhost:${server.address().port}`);
+});
+
+// web sockets stuff
+var clients = {};
+const wsPort = 8081;
+var webSocketServer = new WebSocketServer.Server({
+  port: wsPort
+});
+
+webSocketServer.on('connection', function(ws) {
+  var id = Math.random();
+  clients[id] = ws;
+  console.log("a new connection has been established: " + id);
+
+  ws.on('message', function(message) {
+    console.log('message received: ' + message);
+
+    for (var key in clients) {
+      clients[key].send(message);
+    }
+  });
+
+  ws.on('close', function() {
+    console.log('the connection has been closed: ' + id);
+    delete clients[id];
+  });
+  
 });
